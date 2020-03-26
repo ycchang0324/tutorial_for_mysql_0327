418908341a80949141e67f52481d3c7f43881be6
Justin Chang <ycchang0324@gmail.com>
2020-03-26 21:24:15 +0800

    first commit

---
+++ tutorial_1.php
@@ -0,0 +1,22 @@
+<!DOCHTML html>
+<html>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
+
+</body>
+</html>
