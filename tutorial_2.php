40cc775d9bd1edd290d2af521531186cc175010a
Justin Chang <ycchang0324@gmail.com>
2020-03-26 21:54:05 +0800

    2

--- tutorial_2.php
+++ tutorial_2.php
@@ -1,3 +1,4 @@
+
 <?php
 $servername = "localhost:3306";
 $username = "root";
@@ -5,10 +6,18 @@ $password = "root";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
